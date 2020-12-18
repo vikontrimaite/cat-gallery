c44ef9d89be5228f4bb243025f33dd93cb775dac
Vilija Kontrimaite <vikontrimaite@gmail.com>
2020-12-18 11:25:33 +0200

    update functions

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 function upDate(element) {
     document.getElementById('mainBox').innerHTML = element.alt;
+
     document.getElementById('mainBox').style.background = "url('"+element.src+"')"
 }
 
 function unDo() {
-    document.querySelector('#mainBox').style.backgroundImage = "url('')";
-      document.querySelector('#mainBox').innerHTML = "Hover here again to reveal cat's name!";
+    document.getElementById('mainBox').innerHTML = "Hover here again to reveal cat's name!";
+    
+    document.getElementById('mainBox').style.backgroundImage = "url('')";
 }
 
